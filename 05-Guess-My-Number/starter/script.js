0704e72de414014511c5c91fa334bbac5edc6cb9
Sarah <srhmendez@gmail.com>
2021-08-19 16:22:16 -0600

    refactoring

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,77 +4,88 @@ let getRandomNum = function() {
     let randomNum = (Math.trunc(Math.random() * 20) + 1);
     return randomNum;
 }
-let getCurrentScore = () => { return Number(document.querySelector('.score').textContent);};
+let getCurrentScore = () => { return Number(score.textContent);};
 
+let score = document.querySelector('.score');
 let secretNumber = getRandomNum();
-let currentScore = Number(document.querySelector('.score').textContent);
+let currentScore = Number(score.textContent);
 let highScore = Number(document.querySelector('.highscore').textContent);
-let guessInput = document.getElementsByClassName('.guess');
+let guess = document.querySelector('.guess');
+let background = document.querySelector('body');
+let message = document.querySelector('.message');
+let secretNumberDisplay = document.querySelector('.number');
+
+
+
+
 console.log(`This is the Secret Number: ${secretNumber}`)
 
 let checkGuess = function() {
-    const guess = Number(document.querySelector('.guess').value);
+    const guessValue = Number(guess.value);
 
     //When there is no input
-    if (!guess) {
-        document.querySelector('.message').textContent = '🚫 No Number!';
+    if (!guessValue) {
+        message.textContent = '🚫 No Number!';
 
     //when player wins
-    } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = '🎉 You are Correct!';
+    } else if (guessValue === secretNumber) {
+        message.textContent = '🎉 You are Correct!';
 
-        document.querySelector('body').style.backgroundColor = '#60b347';
-        document.querySelector('.number').style.width = '30rem'
-        document.querySelector('.number').textContent = secretNumber;
+        background.style.backgroundColor = '#60b347';
+        secretNumberDisplay.style.width = '30rem'
+        secretNumberDisplay.textContent = secretNumber;
         currentScore = getCurrentScore();
-        if (Number(document.querySelector('.score').textContent) > highScore) {
+        if (Number(score.textContent) > highScore) {
             document.querySelector('.highscore').textContent = currentScore;
         }
         secretNumber = getRandomNum();
         console.log(`The new Secret Number is: ${secretNumber}`);
     //When guess is too low
-    } else if (guess < secretNumber){
+    } else if (guessValue < secretNumber){
         if (currentScore != 0) {
-            document.querySelector('.message').textContent = '📉 Too low! Guess Again.';
+            message.textContent = '📉 Too low! Guess Again.';
             currentScore --
-            document.querySelector('.score').textContent = currentScore;
-            document.querySelector('.guess').value = '';
+            score.textContent = currentScore;
+            guess.value = '';
         } else {
-            document.querySelector('.message').textContent = '🥶 You lost the game.'
+            message.textContent = '🥶 You lost the game.'
         }
 
     //When guess is too high
-    } else if (guess > secretNumber){
+    } else if (guessValue > secretNumber){
         if (currentScore != 0) {
-            document.querySelector('.message').textContent = ' 📈 Too high! Guess Again.'
+            message.textContent = ' 📈 Too high! Guess Again.'
             currentScore --
-            document.querySelector('.score').textContent = currentScore;
-            document.querySelector('.guess').value = '';
+            score.textContent = currentScore;
+            guess.value = '';
         } else {
-            document.querySelector('.message').textContent = '🥶 You lost the game.'
+            message.textContent = '🥶 🤧 No more Attempts Left! You lose.'
         }
     }
 }
 
 
-let resetGame = function() {
-    document.querySelector('body').style.backgroundColor = '#222';
-    document.querySelector('.message').textContent = 'Start guessing...';
-    document.querySelector('.guess').value = '';
-    document.querySelector('.number').textContent = '?';
-    document.querySelector('.score').textContent = 20;
 
-}
 
+// Function to see if the user has input a value (Otherwise the high score will be 20 without playing when you click the Again! button)
 let playAgain = function() {
-    if (document.querySelector('.guess') == false) {
-        console.log('You haven\'t played the game');
+    if (guess == false) {
+        // Do nothing ( make user play to receive High score of 20)
     } else {
         resetGame();
-    
     }
 }
 
+// Function that resets the game without resetting the page, allowing the high score to be updated and carry over from round to round.
+let resetGame = function() {
+    background.style.backgroundColor = '#222';
+    message.textContent = 'Start guessing...';
+    guess.value = '';
+    secretNumberDisplay.textContent = '?';
+    score.textContent = 20;
 
+}
+
+//Buttons 
 document.querySelector('.check').addEventListener('click', checkGuess);
 document.querySelector('.again').addEventListener('click', playAgain);
